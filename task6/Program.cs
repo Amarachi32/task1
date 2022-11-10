85e3248a12a535d5aaefcb6f2332db70e46752e2
IZUEGBUNAM PERPETUA AMARACHI <54138835+Amarachi32@users.noreply.github.com>
2022-11-10 15:23:23 +0100

    Add files via upload

---
+++ task6/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("select word c,o,d,e");
+string input = Console.ReadLine();
+
+    switch(input.ToLower()){
+    case "c":
+    Console.WriteLine("c# books");
+    break;
+
+    case "o":
+    Console.WriteLine(".Net core books");
+    break;
+
+    case "d":
+    Console.WriteLine("clean coding books");
+    break;
+
+    case "e":
+    Console.WriteLine("EF core books");
+    break;
+    default:
+     Console.WriteLine("does not exist");
+    break;
+}
+
+
+
