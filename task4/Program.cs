6eeb302bdbeb15b86ec0989d5b1820096da097e4
IZUEGBUNAM PERPETUA AMARACHI <54138835+Amarachi32@users.noreply.github.com>
2022-11-10 15:02:45 +0100

    Add files via upload

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Console.WriteLine("enter number");
+// int num = Convert.ToInt32(Console.ReadLine());
+int[] myNum = {1,2,3,4,5,6,7,8,9};
+// Console.WriteLine(num);
+foreach(int num in myNum){
+    
+    if(num == 8){
+        Console.WriteLine("break");
+        break;
+        
+    }
+    if(num == 2){
+        Console.WriteLine("skip 2");
+        continue;
+        
+    }
+    Console.WriteLine(num);
+    
+}
